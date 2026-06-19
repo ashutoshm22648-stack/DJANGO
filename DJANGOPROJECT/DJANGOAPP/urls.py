aaf516650a4b6ac3724c15439353107fa9d4b893
ashutoshm22648-stack <ashutoshm22648@gmail.com>
2026-06-19 14:47:45 +0530

    html pages added

--- DJANGOPROJECT/DJANGOAPP/urls.py
+++ DJANGOPROJECT/DJANGOAPP/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 
 from DJANGOAPP.views import view1
+from DJANGOAPP.views import *
+
+
 
 urlpatterns = [
-    path('view1/', view1,name='view1'),
+    path('view1/', view1,name='view1'), 
+    path('sum/', sum,name='sum'),
+    path('now/', date, name='now'),
+    path('home/', home, name='home'),
+    path('about/', aboutus, name='about'),
+
+
 ]
--- DJANGOPROJECT/DJANGOAPP/views.py
+++ DJANGOPROJECT/DJANGOAPP/views.py
@@ -1,6 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
+
+def home(request):
+    return render(request,'homePage.html')
+
+def aboutus(request):
+    return render(request,'aboutPage.html')
+
 def view1(request):
     return HttpResponse("hello,World! This is view1.")
+
+def sum(request):
+    a = 12
+    b = 56
+    result = a + b
+    return HttpResponse(f"The sum of {a} and {b} is {result}.")
+
+# write a django view to print system date of the browser
+def date(request):
+    now1 =(datetime.datetime.now())
+    return HttpResponse(now1)
